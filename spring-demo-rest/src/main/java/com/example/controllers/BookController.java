b9574f2aaea0301ddf0a00624db341b8fc0e568f
Shristihub <spriyamathan@shristitechlabs.com>
2020-04-18 21:50:12 +0530

    Add files via upload

--- spring-demo-rest/src/main/java/com/example/controllers/BookController.java
+++ spring-demo-rest/src/main/java/com/example/controllers/BookController.java
@@ -1,13 +1,34 @@
 package com.example.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bean.Book;
+import com.example.service.BookService;
 
 @RestController
 public class BookController {
 
-	@RequestMapping("/greet")
+	@Autowired
+	BookService bookService;
+	
+	@GetMapping("/greet")
 	public String greet() {
-		return "Welcome to Spring";
+		return "welcome";
 	}
+	
+	@GetMapping("/book/{bookId}")
+	public Book findById(@PathVariable("bookId")int id) {
+	 Book book = bookService.getBookbyId(id);	
+		return book;
+	}
+	@GetMapping("/books-by-author/{author}")
+	public List<Book> findById(@PathVariable("author")String author) {
+	 return bookService.getBookByAuthor(author);	
+	}
+	
 }
